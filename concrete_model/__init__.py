4022db98e2749eaf8c012c830b8125c2a89459a9
etamsen <erik.tamsen@bam.de>
2022-03-29 09:19:10 +0200

    restructured material problems

--- concrete_model/__init__.py
+++ concrete_model/__init__.py
@@ -3,5 +3,5 @@ from concrete_model.experimental_setups.concrete_column import ConcreteColumnExp
 from concrete_model.experimental_setups.concrete_cube import ConcreteCubeExperiment
 from concrete_model.experimental_setups.concrete_beam import ConcreteBeamExperiment
 from concrete_model.experimental_setups.minimal_cube import MinimalCubeExperiment
-from concrete_model.material_problems import ConcreteThermoMechanical
+from concrete_model.material_problems.concrete_thermo_mechanical import ConcreteThermoMechanical
 from concrete_model.helpers import Parameters
