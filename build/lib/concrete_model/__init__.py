b4c3f1a257496836fd0f0bba41e5e85227ba4e3a
etamsen <erik.tamsen@bam.de>
2022-04-25 11:45:07 +0200

    messing around with conda and pypi

---
+++ build/lib/concrete_model/__init__.py
@@ -0,0 +1,10 @@
+import concrete_model.sensors
+from concrete_model.experimental_setups.concrete_column import ConcreteColumnExperiment
+from concrete_model.experimental_setups.concrete_cube import ConcreteCubeExperiment
+from concrete_model.experimental_setups.concrete_beam import ConcreteBeamExperiment
+from concrete_model.experimental_setups.minimal_cube import MinimalCubeExperiment
+from concrete_model.material_problems.concrete_thermo_mechanical import ConcreteThermoMechanical
+from concrete_model.helpers import Parameters
+
+# this is the only place to define the package version; 
+__version__ = "0.1.0"
