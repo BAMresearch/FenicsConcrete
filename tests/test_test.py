dddf519a4688d9fa4d3e7bc25aeb47f3c95ac79c
eriktamsen <erik.tamsen@web.de>
2022-07-05 16:06:24 +0200

    testing simple test

---
+++ tests/test_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+# import warnings
+# from ffc.quadrature.deprecation \
+#     import QuadratureRepresentationDeprecationWarning
+# warnings.simplefilter("ignore", QuadratureRepresentationDeprecationWarning)
+
+def test_test():
+    print('Moin')
+
+
+    assert 'this' == 'this'
