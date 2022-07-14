484537ad3192e066d8e21a5ee9a2c9825345b06d
araderma <annika.robens-radermacher@bam.de>
2022-07-14 11:50:11 +0200

    add residula force sensor option in ThixModel add additional test example for density load with residual force sensor

--- tests/test_concretethixmodel.py
+++ tests/test_concretethixmodel.py
@@ -146,34 +146,30 @@ def test_density_thix_2D():
     parameters['dt'] = 1 * 60  # 0.5 min step
 
     # sensor
-    sensor01 = fenics_concrete.sensors.StrainSensor(df.Point(0.5,0.0))
-    sensor02 = fenics_concrete.sensors.ReactionForceSensorBottom()
-    sensor03 = fenics_concrete.sensors.StrainSensor(df.Point(0.5,0.5))
+    sensor01 = fenics_concrete.sensors.StrainSensor(df.Point(0.5,0.0)) #1.strainsensor middle bottom
+    sensor02 = fenics_concrete.sensors.StrainSensor(df.Point(0.5,0.5)) #2.strainsensor middle middle
+    sensor03 = fenics_concrete.sensors.ReactionForceSensorBottom()
 
     age0_given = df.Expression('age_zero', age_zero=parameters['age_zero'], degree=0)
     prop2D = setup_test(parameters,[sensor01,sensor02,sensor03],age0_given)
 
     # tests
-    # get stresses and strains at the end
-    print('strain sensor',prop2D.sensors[sensor01.name].data[-1])
-    print('reaction force?', prop2D.sensors[sensor02.name].data[-1])
-    force_bottom = prop2D.sensors[sensor02.name].data[-1]
-    print('strain?', parameters['density']*prop2D.p.g/prop2D.p.E_0)
-    print('force bottom', -parameters['density']*prop2D.p.g*1*1)
-    # strain_yy = strain_T[-1]
-    # strain_xx = strain_T[0]
-    #
-    print('strain sensor', prop2D.sensors[sensor03.name].data[:])
+    # get stresses and strains at the end usually the sensor names are the sensor class name if same kind then numbered!!
+    # print(prop2D.sensors.keys()) # available Sensor Names
+    # print('strain sensor',prop2D.sensors[list(prop2D.sensors.keys())[0]].data[-1]) # or "StrainSensor" = sensor01.name
+    # print('strain sensor', prop2D.sensors[list(prop2D.sensors.keys())[1]].data[-1]) # or "StrainSensor" != sensor02.name !
+    strain_bottom = prop2D.sensors[list(prop2D.sensors.keys())[0]].data[-1][-1] # yy
+    strain_middle = prop2D.sensors[list(prop2D.sensors.keys())[1]].data[-1][-1] # yy
 
-    assert force_bottom == pytest.approx(-parameters['density']*prop2D.p.g*1*1)
-    # assert strain_xx == pytest.approx(-prop2D.p.nu*prop2D.p.u_bc)
+    # print('reaction force', prop2D.sensors[list(prop2D.sensors.keys())[-1]].data[-1]) # or "ReactionForceSensor" = sensor03.name
+    force_bottom = prop2D.sensors[sensor03.name].data[-1]
+    # print('strain', -parameters['density']*prop2D.p.g/prop2D.p.E_0)
+    # print('force bottom', -parameters['density']*prop2D.p.g*1*1)
 
-    # sensor_stress_yy = prop2D.sensors[sensor01.name].data[-1][-1] # yy or zz direction depending on problem dimension
-    # # expected stress value
-    # age_end = parameters['time'] + parameters['age_zero']
-    # E_end = prop2D.p.E_0 + prop2D.p.R_E * prop2D.p.t_f + prop2D.p.A_E * (age_end - prop2D.p.t_f)
-    # assert sensor_stress_yy == pytest.approx(
-    #     parameters['u_bc'] / 1 * E_end)  # compare computed stress with the E*strain
+    assert force_bottom == pytest.approx(-parameters['density']*prop2D.p.g*1*1) # dead load of full structure
+    assert strain_middle == pytest.approx(strain_bottom / 2., abs=1e-4) # linear increase of strain over heigth
+    assert strain_bottom == pytest.approx(-parameters['density']*prop2D.p.g/prop2D.p.E_0, abs=1e-4)
+
 
 
 if __name__ == '__main__':
@@ -183,7 +179,7 @@ if __name__ == '__main__':
 #
 #     test_displ_thix_2D()
 
-    test_density_thix_2D()
+    # test_density_thix_2D()
 
 
 
