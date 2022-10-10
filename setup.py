4d888f14bb9b8e9dba8fe3160b9ae33e4a510473
Erik Tamsen <git@eriktamsen.com>
2022-10-10 14:26:40 +0200

    changing version number as a test

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = 'https://github.com/BAMresearch/FenicsConcrete'
 EMAIL = 'erik.tamsen@bam.de'
 AUTHOR = 'Erik Tamsen'
 REQUIRES_PYTHON = '>=3.9.0'
-VERSION = '0.5.2'
+VERSION = '0.6.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = []
